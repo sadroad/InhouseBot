6809953d5a6bf1fe100c0f0d4e0f5281dde2cd71
1alexvillablanca@gmail.com <1alexvillablanca@gmail.com>
2022-06-16 04:40:37 -0700

    Ability to display/update embed and leave a single role

--- src/commands/queue.rs
+++ src/commands/queue.rs
@@ -1,62 +1,82 @@
-use crate::Prefix;
+use crate::{Prefix, QueueEmbed,QueueChannel};
 use crate::lib::inhouse::*;
 
 use serenity::framework::standard::{Args, CommandResult, macros::command};
 use tokio::time::{sleep, Duration};
+use serenity::model::id::{MessageId};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 
 #[command]
 pub async fn queue(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    let prefix;
-    {
-        let data = ctx.data.read().await;
-        prefix = data.get::<Prefix>().unwrap().clone();
-    }
-    if args.len() != 1 {
-        let response = msg.reply_ping(&ctx.http, &format!("Usage: {}queue <role>", prefix)).await?;
-        sleep(Duration::from_secs(3)).await;
-        response.delete(&ctx.http).await?;
-    } else {
-        let role = args.single::<String>().unwrap();
-        {
-            let data = ctx.data.write().await;
-            let queue = data.get::<QueueManager>().unwrap();
-            let player = Player {
-                discord_id: msg.author.id.to_string(),
-                discord_name: msg.author.name.to_string(),
-            };
-            let mut queue = queue.lock().await;
-            if let Err(e) = queue.queue_player(player, &role) {
-                let response = msg.reply_ping(&ctx.http, &format!("Error: {}", e)).await?;
-                sleep(Duration::from_secs(3)).await;
-                response.delete(&ctx.http).await?;
+    if correct_channel(ctx, msg).await {
+        if args.len() != 1 {
+            let prefix;
+            {
+                let data = ctx.data.read().await;
+                prefix = data.get::<Prefix>().unwrap().clone();
             }
-        }
-        display(ctx,msg).await;
+            let response = msg.reply_mention(&ctx.http, &format!("Usage: {}queue <role>", prefix)).await?;
+            sleep(Duration::from_secs(3)).await;
+            response.delete(&ctx.http).await?;
+        } else {
+            let role = args.single::<String>().unwrap();
+            {
+                let data = ctx.data.write().await;
+                let queue = data.get::<QueueManager>().unwrap();
+                let player = Player {
+                    discord_id: msg.author.id.to_string(),
+                    discord_name: msg.author.name.to_string(),
+                };
+                let mut queue = queue.lock().await;
+                if let Err(e) = queue.queue_player(player, &role) {
+                    let response = msg.reply_mention(&ctx.http, &format!("Error: {}", e)).await?;
+                    sleep(Duration::from_secs(3)).await;
+                    response.delete(&ctx.http).await?;
+                }
+            }
+            display(ctx,msg).await;
+        }    
     }
     msg.delete(&ctx.http).await?;
     Ok(())
 }
 
 #[command]
-pub async fn leave(ctx: &Context, msg: &Message) -> CommandResult {
-    //TODO implement support for leaving specific role
-    {
-        let data = ctx.data.write().await;
-        let queue = data.get::<QueueManager>().unwrap();
-        let mut queue = queue.lock().await;
-        if let Err(e) = queue.leave_queue(msg.author.id.to_string()) {
-            let response = msg.reply_ping(&ctx.http, &format!("Error: {}", e)).await?;
+pub async fn leave(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    if correct_channel(ctx, msg).await {
+        if args.len() > 1 {
+            let prefix;
+            {
+                let data = ctx.data.read().await;
+                prefix = data.get::<Prefix>().unwrap().clone();
+            }
+            let response = msg.reply_mention(&ctx.http, &format!("Usage: {}leave <role?>", prefix)).await?;
             sleep(Duration::from_secs(3)).await;
             response.delete(&ctx.http).await?;
+        } else {
+            let role = if args.len() == 1 {
+                args.single::<String>().unwrap()
+            } else {
+                "".to_string()
+            };
+            {
+                let data = ctx.data.write().await;
+                let queue = data.get::<QueueManager>().unwrap();
+                let mut queue = queue.lock().await;
+                if let Err(e) = queue.leave_queue(msg.author.id.to_string().as_str(), &role) {
+                    let response = msg.reply_mention(&ctx.http, &format!("Error: {}", e)).await?;
+                    sleep(Duration::from_secs(3)).await;
+                    response.delete(&ctx.http).await?;
+                }
+            }
+            display(ctx,msg).await;
         }
     }
-    display(ctx,msg).await;
     Ok(())
 }
 
-async fn display(ctx: &Context, msg: &Message){
+pub async fn display(ctx: &Context, msg: &Message){
     let prefix;
     {
         let data = ctx.data.read().await;
@@ -69,14 +89,45 @@ async fn display(ctx: &Context, msg: &Message){
         let queue = queue.lock().await;
         body = queue.display();
     }
-    let response = msg
-        .channel_id
-        .send_message(&ctx, |m| {
-            m.embed(|e| {
-                e.fields(vec![
-                    ("Queue", body,true)
-                ])
-                .footer(|f| f.text(&format!("Use {}queue <role> to join or {}leave to leave | All non-queue messages are deleted", prefix, prefix)))
-            })
-    }).await.unwrap();
+    {
+        let mut data = ctx.data.write().await;
+        let queue = data.get_mut::<QueueEmbed>().unwrap();
+        if *queue == MessageId(0) {
+            let response = msg
+                .channel_id
+                .send_message(&ctx, |m| {
+                    m.embed(|e| {
+                        e.fields(vec![
+                            ("Queue", body,true)
+                        ])
+                        .footer(|f| f.text(&format!("Use {}queue <role> to join or {}leave to leave | All non-queue messages are deleted", prefix, prefix)))
+                    })
+            }).await.unwrap();
+            let response = response.id;
+            *queue = response;
+        } else {
+            msg
+            .channel_id
+            .edit_message(&ctx, *queue, |m| {
+                m.embed(|e| {
+                    e.fields(vec![
+                        ("Queue", body,true)
+                    ])
+                    .footer(|f| f.text(&format!("Use {}queue <role> to join or {}leave to leave | All non-queue messages are deleted", prefix, prefix)))
+                })
+            }).await.unwrap();
+        }
+    }
+
+}
+
+async fn correct_channel(ctx: &Context, msg: &Message) -> bool{
+    {
+        let data = ctx.data.read().await;
+        let channel_id = data.get::<QueueChannel>().unwrap();
+        if channel_id != &msg.channel_id {
+            return false;
+        }
+    }
+    return true;
 }
