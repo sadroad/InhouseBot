68e9f7b8e16f9dc98e517a1c5baebe49290baa6e
1alexvillablanca@gmail.com <1alexvillablanca@gmail.com>
2022-06-16 04:41:15 -0700

    Admin's can unmark channel and channels are cleared on mark/unmark

--- src/commands/admin.rs
+++ src/commands/admin.rs
@@ -1,4 +1,6 @@
-use crate::{QueueChannel, Prefix};
+use crate::{QueueChannel, Prefix, QueueEmbed};
+
+use super::queue::display;
 
 use serenity::framework::standard::{CommandResult, macros::command};
 use tokio::time::{sleep, Duration};
@@ -43,10 +45,14 @@ async fn mark (ctx: &Context, msg: &Message) -> CommandResult {
                         let mut data = ctx.data.write().await;
                         let queue_channel = data.get_mut::<QueueChannel>().unwrap();
                         *queue_channel = msg.channel_id;
+                        let queue = data.get_mut::<QueueEmbed>().unwrap();
+                        *queue = MessageId(0);
                     }
                     let response = msg.reply_ping(&ctx.http, "Queue channel marked.").await?;
                     sleep(Duration::from_secs(3)).await;
                     response.delete(&ctx.http).await?;
+                    clear_channel(ctx, msg.channel_id).await;
+                    display(ctx, msg).await;
                 } else {
                     let response = msg.reply_ping(&ctx.http, "Cancelled.").await?;
                     sleep(Duration::from_secs(3)).await;
@@ -65,6 +71,48 @@ async fn mark (ctx: &Context, msg: &Message) -> CommandResult {
 }
 
 #[command]
-async fn unmark (_ctx: &Context, _msg: &Message) -> CommandResult {
+async fn unmark (ctx: &Context, msg: &Message) -> CommandResult {
+    let channel_id;
+    {
+        let data = ctx.data.read().await;
+        channel_id = data.get::<QueueChannel>().unwrap().clone();
+    }
+    if channel_id != ChannelId(0) {
+        if msg.channel_id != channel_id {
+            let prefix;
+            {
+                let data = ctx.data.read().await;
+                prefix = data.get::<Prefix>().unwrap().clone();
+            }
+            let response = msg.reply_ping(&ctx.http, &format!("You must first mark the prior queue channel. Use {}admin mark", prefix)).await?;
+            sleep(Duration::from_secs(3)).await;
+            response.delete(&ctx.http).await?;
+        } else {
+            {
+                let mut data = ctx.data.write().await;
+                let queue_channel = data.get_mut::<QueueChannel>().unwrap();
+                *queue_channel = ChannelId(0);
+                let queue = data.get_mut::<QueueEmbed>().unwrap();
+                *queue = MessageId(0);
+            }
+            let response = msg.reply_ping(&ctx.http, "Queue channel unmarked.").await?;
+            sleep(Duration::from_secs(3)).await;
+            response.delete(&ctx.http).await?;
+            clear_channel(ctx, msg.channel_id).await;
+            dbg!("Helo");
+        }
+    } else {
+        let response = msg.reply_ping(&ctx.http, "No queue channel marked.").await?;
+        sleep(Duration::from_secs(3)).await;
+        response.delete(&ctx.http).await?;
+    }
+    msg.delete(&ctx.http).await?;
     Ok(())
+}
+
+async fn clear_channel( ctx: &Context, channel: ChannelId) {
+    let messages = channel.messages(&ctx.http, |m| m).await.unwrap();
+    for message in messages {
+        message.delete(&ctx.http).await.unwrap();
+    }
 }
